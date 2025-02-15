6e3cbe0c814dc7de72f2973d9b85c3787290582d
SVPraveen1 <shyamkaruparthi@gmail.com>
2025-02-15 22:49:43 +0530

    Header done

--- app/page.js
+++ app/page.js
@@ -2,9 +2,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return  <div>Started new project
-    <br />
-    <Button>Sart</Button>
+  return  <div>
   </div>
       
       
