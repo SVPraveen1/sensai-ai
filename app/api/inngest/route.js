5979f8aa5c121283c8281fff6b154c8da980545f
SVPraveen1 <shyamkaruparthi@gmail.com>
2025-02-17 22:50:00 +0530

    Cron job with inngest done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,12 @@
 import { serve } from "inngest/next";
-
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/functions";
-export const { GET, POST, PUT } = serve({
+
+const handler = serve({
   client: inngest,
   functions: [generateIndustryInsights],
 });
 
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
